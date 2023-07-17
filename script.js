a2eaf8c2efe6da828f1f12be1fc4b2eb3bf4c2b9
toxicmix <72023909+toxicmix@users.noreply.github.com>
2023-07-16 21:36:51 -0500

    updated api key so it calles the api and receives data

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Global variables
 var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = 'e6a6933721cea816776aca0541933b06';
 
 // DOM element references
 var searchForm = document.querySelector('#search-form');
